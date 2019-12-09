bac0183daf05a72102a8b5b1ac22512c696e3645
liuwang <330141830@qq.com>
2019-12-09 16:04:03 +0800

    重新编写leetcodeC++

---
+++ leetcodeC++/leetcodeC++/leetcodeC++.cpp
@@ -0,0 +1,17 @@
+// leetcodeC++.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Test.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	/*测试 addTwoNumbers*******************/
+	Test::TestAddTwoNumbers();
+	/*************************************/
+
+	system("pause");
+	return 0;
+}
+
